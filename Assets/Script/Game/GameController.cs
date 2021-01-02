5130fb350c121390bf97f3d5de2928278cde695a
Billy <billy.bouchard03@gmail.com>
2021-01-02 11:10:59 -0500

    add wave controller

---
+++ Assets/Script/Game/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private WaveController _wave_controller;
+
+    public List<IPlayer> _players;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnValidate()
+    {
+        if (_wave_controller == null)
+        {
+            _wave_controller = gameObject.AddComponent<WaveController>();
+        }
+    }
+}
---
+++ Assets/Script/Game/WaveController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour
+{
+    private float _timer;
+    private int _wave_number;
+
+    [SerializeField]
+    private float _wave_trigger_time;
+
+    public delegate void DelegateWaveLaunch();
+
+    public List<DelegateWaveLaunch> _wave_laucher_delegates;
+
+    void Start()
+    {
+        _timer = 0;
+        _wave_number = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer >= _wave_trigger_time)
+        {
+
+        }
+    }
+
+    public void RegisterWaveLauncher()
+    {
+
+    }
+}
